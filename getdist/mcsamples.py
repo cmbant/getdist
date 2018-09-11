e1f0c18a7eb30df4aabded0dcae485fcdb095942
Antony Lewis <antony@cosmologist.info>
2018-09-11 10:02:28 +0100

    fix MCSamples.getMargeStats() with include_bestfit

--- getdist/mcsamples.py
+++ getdist/mcsamples.py
@@ -2069,7 +2069,7 @@ class MCSamples(Chains):
         m.limits = self.contours
         m.names = self.paramNames.names
         if include_bestfit:
-            return self.getBestFit()
+            m.addBestFit(self.getBestFit())
         return m
 
     def getLikeStats(self):
