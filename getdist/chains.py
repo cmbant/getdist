85ca2ff71463d7368906215a7fd255e13d9113d5
torradocacho <JesusTorrado@users.noreply.github.com>
2019-10-07 17:53:45 +0200

    cobaya_interface: fix for labels

--- getdist/chains.py
+++ getdist/chains.py
@@ -875,7 +875,8 @@ class Chains(WeightedSamples):
             self.updateRenames(renames)
         # If Cobaya sample and label not set manually via keyword, try to load from yaml
         if ((not getattr(self, "label", False) and
-             paramNamesFile and paramNamesFile.endswith(".yaml"))):
+             isinstance(paramNamesFile, six.string_types) and
+             paramNamesFile.endswith(".yaml"))):
             from getdist.cobaya_interface import get_sample_label
             self.label = get_sample_label(paramNamesFile)
         # Sampler that generated the chain -- assume "mcmc"
