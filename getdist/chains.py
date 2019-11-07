6e3f1d29943ff1dfd30f40295f3ae1f0a6c43958
Antony Lewis <antony@cosmologist.info>
2019-11-07 20:25:14 +0000

    typo

--- getdist/chains.py
+++ getdist/chains.py
@@ -1174,8 +1174,8 @@ class Chains(WeightedSamples):
             ignore_lines = self.ignore_lines
         WSkwargs = {"ignore_rows": ignore_lines,
                     "min_weight_ratio": self.min_weight_ratio}
-        if files_or_samples == []:
-            raise ValueError('files_or_samples is loadChains')
+        if hasattr(files_or_samples, '__len__') and not len(files_or_samples) or files_or_samples is None:
+            raise ValueError('files_or_samples empty in loadChains')
         if isinstance(files_or_samples, six.string_types) or isinstance(files_or_samples[0], six.string_types):
             # From files
             if weights is not None or loglikes is not None:
