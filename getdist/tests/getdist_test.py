2715c8dc18478e7bfbf2e36168c516af45a17ee8
chris <chris@ringeval.com>
2021-06-16 13:45:27 +0200

    Add test for getRawNDDensity()

--- getdist/tests/getdist_test.py
+++ getdist/tests/getdist_test.py
@@ -146,6 +146,21 @@ class GetDistTest(unittest.TestCase):
         d2 = samps.get2DDensity('x', 'y')
         self.assertTrue(np.allclose(d.P, d2.P[::-1, ::], atol=1e-5))
 
+
+    def testNDDensity(self):
+        # test that getRawNDDensity is close to get2DDensity (Note
+        # that RawND has no smoothing and this is not expected to be
+        # machine-precision accurate)
+
+        samps = Gaussian2D([0, 0], np.diagflat([1, 1]), xmin=-2, xmax=2, ymin=-2, ymax=2).MCSamples(1000000,
+                                                                                                   random_state=10)
+        #not too much bins to get enough stat on RawNDDensity
+        d2D = samps.get2DDensity('x', 'y',fine_bins_2D=10,smooth_scale_2D=1,boundary_correction_order=1)
+        dND = samps.getRawNDDensity(['x','y'],num_bins_ND=10,boundary_correction_order=1)
+        
+        self.assertTrue(np.allclose(d2D.P, dND.P,atol=1e-5))
+
+        
     def testLoads(self):
         # test initiating from multiple chain arrays
         samps = []
